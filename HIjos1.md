7d8b0dc19af870f16756f4ce37d6fc02474fc26e
CristianIDL <delgado.lucero.cristian@gmail.com>
2026-03-12 17:33:11 -0600

    feat: subir versión del código sin suspención de procesos

---
+++ HIjos1.md
@@ -0,0 +1,35 @@
+%%PID%%
+
+# Qué es PID
+
+> PID son siglas de Process ID.
+
+
+De esta forma 
+
+_UN compañero preguntó si se podría hacer en C++, el profesor dijo que técnicamente sería posible pero que_
+
+# getpid()
+
+> Es una llamada al sistema que le pide el identificador. 
+
+`getpid()` no requiere un parámetro por que al momento de hacer una interrupción al sistema desde el Proceso A
+
+## Por qué primero muestra al Padre y luego al Hijo?
+
+> Esto tiene algo que ver con el [[Kernel]], este le dice que ejeccute al padre primero. Es como ir con un gurú en tlatemalco o así. Usando un algoritmo interno verificador. La elección depende de las circunstancias alrededor del proceso.
+
+_El profesor comenta que no va a adentrarse porque laa clase no es de [[Sistemas Operativos]]_
+
+
+# getppid()
+
+> `Get Parent Rroces Identification`
+
+_A un compañero y a mi nos salió mal uno de los padres, a lo que el profesor muy serio comentó:_
+
+> "A veces `gcc` está loco."
+
+Tras un rato de meditar el profe llegó a una conclusión.
+
+> Seguramente en algún momento les llegue a pasar a todos
--- Hijos1.c
+++ Hijos1.c
@@ -1,50 +1,24 @@
 #include <stdio.h>
-
-// Esta función es para las entradas su salidas estándar. C
-// Con esta podemos usar el teclado y pantalla
-
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(){
-  pid_t pid;
-  // pid_t es un tipo de dato entero.
-  // es una re definición de int. Pero son lo mismo, solo para Linuxeros de corazón.
-  printf("\n\tHola, soy el proceso padre\n\n");
 
-  /* 
-    # ¿Qué es un printf()?
-    Vas a usar algo en la pantalla. Es una [[Llamada al Sistema]].
-    Decir que es una función puede estar medio equis, pero la respuesta correcta es esa
-  */
-
+  int pid;
   pid = fork();
-  /*
-    > Fork significa "bifurcación".
-    Esto es una llamada al sistema. Llama  la RAM y crea un espacio nuevo conocido como proceso hijo.
-    Entonces el proceso A se convierte en proceso padre
-  */
-
+ 
   if(pid == -1){
-    // Quien programó Fork, estableció que "-1" es error.
     perror("Error en fork");
     exit(-1);
   }
   
-  // # IF MÁGICO
-  // Es un if mágico debido a que ambos bloques se usan.
-  
   if(pid == 0){
-    printf("Hola soy el hijo");
+    printf("Hola soy el proceso hijo y mi pid es %d\n\n", getpid());
+    printf("\nMi padre es %d\n\n", getppid());
   } else {
-    // Este código de acá abajo es del padre. Él nunca ejecutará el del hijo.
-    printf("Hola soy el padre");  
+    printf("Hola soy el proceso padre y mi pid es %d\n\n", getpid());  
   }
   
   return 0;
-
- 
-  /*
-    Yo creo que el sistema operativo se creó a imagen y semejanza del ser humano.
-  */
 
 }
